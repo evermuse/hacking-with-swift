29998277e726745734d5d6a110cc6eb8cb57822b
Bryan Alexander <calbryan@gmail.com>
2016-11-07 17:09:12 -0700

    completed Storm Viewer app
    covered table views, file manager, uiimage, etc

---
+++ Storm Viewer/Storm Viewer/DetailViewController.swift
@@ -0,0 +1,65 @@
+//
+//  DetailViewController.swift
+//  Storm Viewer
+//
+//  Created by Bryan Alexander on 11/7/16.
+//  Copyright © 2016 sarva. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        title = selectedImage
+        
+        if let imageToLoad = selectedImage {
+            
+            imageView.image = UIImage(named: imageToLoad)
+            
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
+    
+        navigationController?.hidesBarsOnTap = true
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+
+        super.viewWillDisappear(animated)
+        
+        navigationController?.hidesBarsOnTap = false
+        
+    }
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
