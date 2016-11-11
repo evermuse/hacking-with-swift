d8a8c378f336347b03be1ea7d3f9403dd8e3cc0d
Bryan Alexander <calbryan@gmail.com>
2016-11-11 09:13:16 -0700

    completed the ActivityViewController exercise

---
+++ ActivityViewController/Storm Viewer/DetailViewController.swift
@@ -0,0 +1,75 @@
+//
+//  DetailViewController.swift
+//  Storm Viewer
+//
+//  Created by Bryan Alexander on 11/7/16.
+//  Copyright © 2016 sarva. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        title = selectedImage
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
+        if let imageToLoad = selectedImage {
+            
+            imageView.image = UIImage(named: imageToLoad)
+            
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
+    
+        navigationController?.hidesBarsOnTap = true
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+
+        super.viewWillDisappear(animated)
+        
+        navigationController?.hidesBarsOnTap = false
+        
+    }
+    
+    func shareTapped() {
+        
+        let vc = UIActivityViewController(activityItems: [imageView.image!], applicationActivities: []) // we know the image exists - !
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+        
+    }
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
