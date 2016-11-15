4654143c85fce4ac358b7cf44e0bca7b31bad63c
Bryan Alexander <calbryan@gmail.com>
2016-11-14 20:26:00 -0700

    storyboard for Swifty Words app completed

---
+++ Swifty Words/Swifty Words/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Swifty Words
+//
+//  Created by Bryan Alexander on 11/14/16.
+//  Copyright © 2016 Sarva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var cluesLabel: UILabel!
+    @IBOutlet var answersLabel: UILabel!
+    @IBOutlet var currentAnswer: UITextField!
+    @IBOutlet var scoreLabel: UILabel!
+    
+    @IBAction func submitTapped(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func clearTapped(_ sender: Any) {
+        
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
